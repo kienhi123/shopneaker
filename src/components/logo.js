823b2e161a08e39ace5c2c274ca046bf382ceb8a
kienhi123 <kien123@gmail.com>
2022-01-12 21:38:57 +0700

    fix:lab2

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,7 +2,7 @@ const logo = {
     render() {
         return /* html */ ` <div class="bg-sky-900 py-4">
         <a href="#">
-          <img src="image/FPT_Polytechnic.png" width="150px" height="250px" alt="" class="mx-auto" />
+          <img src="https://picsum.photos/220/100" alt="" class="mx-auto" />
         </a>
       </div>`;
     },
